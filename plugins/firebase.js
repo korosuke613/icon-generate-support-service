d984c153a9083041c886cb5ba90f4f341659606e
Futa Hirakoba <korosuke613613@gmail.com>
2019-12-21 14:50:29 +0000

    delete key

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -3,14 +3,6 @@ import 'firebase/analytics'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyA9mJD8l5d0XW0CHRn8GZUoIVIggWrae5Y',
-  authDomain: 'aikon-eaf3a.firebaseapp.com',
-  databaseURL: 'https://aikon-eaf3a.firebaseio.com',
-  projectId: 'aikon-eaf3a',
-  storageBucket: 'aikon-eaf3a.appspot.com',
-  messagingSenderId: '1031502130478',
-  appId: '1:1031502130478:web:551f7f73cdc8b797fadebf',
-  measurementId: 'G-S017VC36Y1'
 }
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
