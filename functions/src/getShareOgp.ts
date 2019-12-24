14dd12b751985fee25cae880dd97bdb8106d8a86
Futa Hirakoba <korosuke613613@gmail.com>
2019-12-24 15:41:04 +0000

    svg画像の合成に成功

--- functions/src/getShareOgp.ts
+++ functions/src/getShareOgp.ts
@@ -2,6 +2,8 @@ const functions = require('firebase-functions')
 const svg2img = require('svg2img');
 const fs = require('fs');
 const image2base64 = require('image-to-base64');
+const sharp = require("sharp")
+const Canvas = require('canvas'); // node-canvasをrequireする
 
 module.exports = functions.https.onRequest(async (request: any, response: any) => {
   const params = {
@@ -18,20 +20,41 @@ module.exports = functions.https.onRequest(async (request: any, response: any) =
   response.send(url)
 });
 
+const gousei = async () => {
+  // Canvas.Image() メソッドでImg要素を作り、
+  // srcに受け取ったファイルのパスをセットする
+  const baseImg = new Canvas.Image();
+  baseImg.src = await fs.readFileSync('dummy.png');
+  const overImg = new Canvas.Image();
+  overImg.src = await fs.readFileSync('new-file.png');
+
+  // Canvasを初期化する
+  const canvas = Canvas.createCanvas(baseImg.width, baseImg.height);
+  const ctx = canvas.getContext('2d');
+
+  // Canvas上に2つの画像を描画する
+  await ctx.drawImage(baseImg, 0, 0);
+  await ctx.drawImage(overImg, (baseImg.width - overImg.width) / 2, (baseImg.height - overImg.height) / 2);
+
+  const buf = await canvas.toBuffer();
+  await fs.writeFileSync("test.png", buf);
+}
+
 const getSvg = async (url: string, fileName: string) => {
   let svgBase64 = ''
   await image2base64(url) // URLの日本語エスケープ含む
-  .then(
-    (res: string) => {
-      svgBase64 = res
-    }
-  )
-  await svg2img(
-    `data:image/svg+xml;base64,${svgBase64}`,
-    {width:600, height:314, preserveAspectRatio:'xMinYMid meet'},
-    (error: any, buffer: any) => {
-      fs.writeFileSync(fileName, buffer);
-    });
+    .then(
+      (res: string) => {
+        svgBase64 = res
+      }
+    )
+
+  await sharp(Buffer.from(svgBase64, 'base64'), { density: 2400 })
+    .resize({ width: 400, fit: "inside" })
+    .png()
+    .toFile("new-file.png")
+
+  gousei()
 }
 
 const getTransUrl = (params: any) => {
