a80a14a4848992196397ae3e7110f1bc68d995ef
Futa Hirakoba <korosuke613613@gmail.com>
2019-12-29 16:54:45 +0900

    リファクタリング

--- functions/src/common.ts
+++ functions/src/common.ts
@@ -1,5 +1,52 @@
+const fs = require('fs');
+const Canvas = require('canvas'); // node-canvasをrequireする
+const puppeteer = require('puppeteer');
+
 exports.getTransUrl = (params: any) => {
   const url = `https://img.shields.io/static/v1?label=${params.label}&message=${params.message}&color=${params.color}&style=${params.style}&logo=${params.logo}&logoColor=${params.logoColor}`
   return url
 }
 
+exports.getSvg = async (url: string, fileName: string) => {
+  await (async () => {
+   const browser = await puppeteer.launch({args: ['--no-sandbox']});
+   const page = await browser.newPage();
+   await page.goto(url);
+   await page.setViewport({width: 300, height: 300, deviceScaleFactor:2.5});
+ 
+   const clip = await page.evaluate((s: any) => {
+     const el = document.querySelector(s)
+ 
+     // エレメントの高さと位置を取得
+     const { width, height, top: y, left: x } = el.getBoundingClientRect()
+     return { width, height, x, y }
+   }, 'svg')
+ 
+   await page.screenshot({ clip, path: fileName })
+   await browser.close();
+ })();
+   
+   return await gousei(fileName)
+ }
+
+const gousei = async (fileName: string) => {
+  // Canvas.Image() メソッドでImg要素を作り、
+  // srcに受け取ったファイルのパスをセットする
+  const overImg = new Canvas.Image();
+  overImg.src = await fs.readFileSync(fileName);
+
+  const twitterCardSize = {
+    width: 600,
+    height: 314
+  }
+  const canvas = Canvas.createCanvas(twitterCardSize.width, twitterCardSize.height);
+  const ctx = canvas.getContext('2d');
+
+  // Canvas上に画像を描画する
+  await ctx.drawImage(overImg, (twitterCardSize.width - overImg.width) / 2, (twitterCardSize.height - overImg.height) / 2);
+
+  const buf = await canvas.toBuffer();
+  const resultName = `${fileName}.change.png`
+  await fs.writeFileSync(resultName, buf);
+  return resultName
+}
--- functions/src/getShareOgp.ts
+++ functions/src/getShareOgp.ts
@@ -1,11 +1,9 @@
 const functions = require('firebase-functions')
-const admin = require('firebase-admin')
 import * as crypto from "crypto";
-
+const common = require("./common");
+const admin = require('firebase-admin')
 const storage = admin.storage()
-const fs = require('fs');
-const Canvas = require('canvas'); // node-canvasをrequireする
-const puppeteer = require('puppeteer');
+
 const os = require('os');
 const path = require('path')
 
@@ -22,15 +20,15 @@ module.exports = functions.runWith({memory: '1GB'}).https.onRequest(async (reque
   const tmpdir = os.tmpdir();
   const filePath = path.join(tmpdir, 'icon.png')
 
-  const url = getTransUrl(params)
-  const tempLocalFile = await getSvg(url, filePath)
+  const url = common.getTransUrl(params)
+  const tempLocalFile = await common.getSvg(url, filePath)
   
   const metadata = {
     cacheControl: 'public,max-age=31536000', // 1年キャッシュする
     contentType: 'image/png'
   }
   const shasum = crypto.createHash('sha1')
-  shasum.update(url) // ここの引数にハッシュを計算したい文字列を渡す
+  shasum.update(url)
   const uploadName = shasum.digest('hex')
 
   console.log(`upload: ${tempLocalFile}`)
@@ -38,68 +36,6 @@ module.exports = functions.runWith({memory: '1GB'}).https.onRequest(async (reque
     destination: `cards/${uploadName}.png`,
     metadata
   })
-  let imageUrl = ''
-
-  console.log('download')
-
-  await storage.bucket().file(`cards/${uploadName}.png`).getSignedUrl({
-    action: 'read',
-    expires: '03-17-2025'
-  },  (err: any, signedUrl: any) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-      imageUrl = signedUrl
-      response.send(imageUrl)
-  })
+  response.send('Success')
 });
 
-const gousei = async (fileName: string) => {
-  // Canvas.Image() メソッドでImg要素を作り、
-  // srcに受け取ったファイルのパスをセットする
-  const overImg = new Canvas.Image();
-  overImg.src = await fs.readFileSync(fileName);
-
-  const twitterCardSize = {
-    width: 600,
-    height: 314
-  }
-  const canvas = Canvas.createCanvas(twitterCardSize.width, twitterCardSize.height);
-  const ctx = canvas.getContext('2d');
-
-  // Canvas上に画像を描画する
-  await ctx.drawImage(overImg, (twitterCardSize.width - overImg.width) / 2, (twitterCardSize.height - overImg.height) / 2);
-
-  const buf = await canvas.toBuffer();
-  const resultName = `${fileName}.change.png`
-  await fs.writeFileSync(resultName, buf);
-  return resultName
-}
-
-const getSvg = async (url: string, fileName: string) => {
- await (async () => {
-  const browser = await puppeteer.launch({args: ['--no-sandbox']});
-  const page = await browser.newPage();
-  await page.goto(url);
-  await page.setViewport({width: 300, height: 300, deviceScaleFactor:2.5});
-
-  const clip = await page.evaluate((s: any) => {
-    const el = document.querySelector(s)
-
-    // エレメントの高さと位置を取得
-    const { width, height, top: y, left: x } = el.getBoundingClientRect()
-    return { width, height, x, y }
-  }, 'svg')
-
-  await page.screenshot({ clip, path: fileName })
-  await browser.close();
-})();
-  
-  return await gousei(fileName)
-}
-
-const getTransUrl = (params: any) => {
-  const url = `https://img.shields.io/static/v1?label=${params.label}&message=${params.message}&color=${params.color}&style=${params.style}&logo=${params.logo}&logoColor=${params.logoColor}`
-  return url
-}
