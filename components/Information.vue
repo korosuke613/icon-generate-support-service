bead6040e56c022b719bfd41a3761ab738d2d187
Futa Hirakoba <korosuke613613@gmail.com>
2020-01-05 07:33:41 +0900

    折りたたみする

--- components/Information.vue
+++ components/Information.vue
@@ -5,51 +5,67 @@
     <v-card-text>
       <v-row>
         <v-col cols="12">
-          <v-text-field
+          <v-textarea
             :value="getMarkdown()"
             label="Markdown"
             readonly
             outlined
+            no-resize="true"
             onclick="this.select();"
           />
         </v-col>
         <v-col cols="12">
-          <v-text-field
+          <v-textarea
             :value="getHtml()"
             label="HTML"
             readonly
             outlined
-            onclick="this.select();"
-          />
-        </v-col>
-        <v-col cols="6" sm="6" md="6" lg="6" xl="6">
-          <v-textarea
-            :value="url"
-            label="Badge URL"
-            readonly
-            outlined
-            onclick="this.select();"
-          />
-        </v-col>
-        <v-col cols="6" sm="6" md="6" lg="6" xl="6">
-          <v-textarea
-            :value="base64"
-            label="BASE64"
-            readonly
-            outlined
-            onclick="this.select();"
-          />
-        </v-col>
-        <v-col cols="6" sm="6" md="6" lg="6" xl="6">
-          <v-textarea
-            :value="sharedUrl"
-            label="Share URL"
-            readonly
-            outlined
+            no-resize="true"
             onclick="this.select();"
           />
         </v-col>
       </v-row>
+      <v-expansion-panels
+        flat="true"
+      >
+        <v-expansion-panel>
+          <v-expansion-panel-header>more</v-expansion-panel-header>
+          <v-expansion-panel-content>
+            <v-row>
+              <v-col cols="6" sm="6" md="6" lg="6" xl="6">
+                <v-textarea
+                  :value="url"
+                  label="Badge URL"
+                  readonly
+                  outlined
+                  no-resize="true"
+                  onclick="this.select();"
+                />
+              </v-col>
+              <v-col cols="6" sm="6" md="6" lg="6" xl="6">
+                <v-textarea
+                  :value="base64"
+                  label="BASE64"
+                  readonly
+                  outlined
+                  no-resize="true"
+                  onclick="this.select();"
+                />
+              </v-col>
+              <v-col cols="6" sm="6" md="6" lg="6" xl="6">
+                <v-textarea
+                  :value="sharedUrl"
+                  label="Share URL"
+                  readonly
+                  outlined
+                  no-resize="true"
+                  onclick="this.select();"
+                />
+              </v-col>
+            </v-row>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
     </v-card-text>
   </v-card>
 </template>
