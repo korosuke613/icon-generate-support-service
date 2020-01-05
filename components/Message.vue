b5224d3e5b4c729aa4bce7d75e87dff8ac3eb3cc
Futa Hirakoba <korosuke613613@gmail.com>
2020-01-05 09:15:08 +0900

    例をいくつか示した

--- components/Message.vue
+++ components/Message.vue
@@ -1,64 +1,14 @@
 <template>
   <v-card>
     <v-card-title class="headline">
-      Welcome to the Vuetify + Nuxt.js template
+      Sample Badges
     </v-card-title>
     <v-card-text>
-      <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-      <p>
-        For more information on Vuetify, check out the <a
-          href="https://vuetifyjs.com"
-          target="_blank"
-        >
-          documentation
-        </a>.
-      </p>
-      <p>
-        If you have questions, please join the official <a
-          href="https://chat.vuetifyjs.com/"
-          target="_blank"
-          title="chat"
-        >
-          discord
-        </a>.
-      </p>
-      <p>
-        Find a bug? Report it on the github <a
-          href="https://github.com/vuetifyjs/vuetify/issues"
-          target="_blank"
-          title="contribute"
-        >
-          issue board
-        </a>.
-      </p>
-      <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-      <div class="text-xs-right">
-        <em><small>&mdash; John Leider</small></em>
-      </div>
-      <hr class="my-3">
-      <a
-        href="https://nuxtjs.org/"
-        target="_blank"
-      >
-        Nuxt Documentation
-      </a>
-      <br>
-      <a
-        href="https://github.com/nuxt/nuxt.js"
-        target="_blank"
-      >
-        Nuxt GitHub
-      </a>
+      <a href="?la=korosuke613&me=Follow%20Me&co=black&st=flat-square&lo=GitHub&lc=none"><img src="https://img.shields.io/static/v1?label=korosuke613&message=Follow%20Me&color=black&style=flat-square&logo=github"></a>
+      <a href="?la=&me=Twitter&co=1DA1F2&st=flat-square&lo=Twitter&lc=white"><img src="https://img.shields.io/static/v1?label=&message=Twitter&color=1DA1F2&style=flat-square&logo=twitter&logoColor=white"></a>
+      <a href="?la=&me=Facebook&co=1877F2&st=flat-square&lo=Facebook&lc=white"><img src="https://img.shields.io/static/v1?label=&message=Facebook&color=1877F2&style=flat-square&logo=facebook&logoColor=white"></a>
+      <a href="?la=新元号は&me=令和であります&co=black&st=social&lo=Travis%20CI&lc=orange"><img src="https://img.shields.io/static/v1?label=%E6%96%B0%E5%85%83%E5%8F%B7%E3%81%AF&message=%E4%BB%A4%E5%92%8C%E3%81%A7%E3%81%82%E3%82%8A%E3%81%BE%E3%81%99&color=black&style=social&logo=travis-ci&logoColor=orange"></a>
+      <a href="?la=SuperDry%20極度乾燥&me=（しなさい）&co=red&st=plastic&lo=none&lc=orange"><img src="https://img.shields.io/static/v1?label=SuperDry%20%E6%A5%B5%E5%BA%A6%E4%B9%BE%E7%87%A5&message=%EF%BC%88%E3%81%97%E3%81%AA%E3%81%95%E3%81%84%EF%BC%89&color=red&style=plastic&logoColor=orange"></a>
     </v-card-text>
-    <v-card-actions>
-      <v-spacer />
-      <v-btn
-        color="primary"
-        nuxt
-        to="/inspire"
-      >
-        Continue
-      </v-btn>
-    </v-card-actions>
   </v-card>
 </template>
