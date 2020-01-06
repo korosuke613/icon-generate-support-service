2f5123d77fef7f69ba05d6639178d064bfa81a88
Futa Hirakoba <korosuke613613@gmail.com>
2020-01-06 21:47:12 +0900

    add Usage

---
+++ components/Usage.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-card>
+    <v-card-title class="headline">
+      Usage
+    </v-card-title>
+    <v-card-text>
+      BEENOCKER is a site where you can easily generate badges. It also generates code that can be embedded in HTML, Markdown.
+      BEENOCKER uses the URL of Shilds.io generated from the set value.
+    </v-card-text>
+
+    <v-card-title>
+      Sample Badges
+    </v-card-title>
+    <v-card-text>
+      <a href="?la=korosuke613&me=Follow%20Me&co=black&st=flat-square&lo=GitHub&lc=none"><img src="https://img.shields.io/static/v1?label=korosuke613&message=Follow%20Me&color=black&style=flat-square&logo=github"></a>
+      <a href="?la=&me=Twitter&co=1DA1F2&st=flat-square&lo=Twitter&lc=white"><img src="https://img.shields.io/static/v1?label=&message=Twitter&color=1DA1F2&style=flat-square&logo=twitter&logoColor=white"></a>
+      <a href="?la=&me=Facebook&co=1877F2&st=flat-square&lo=Facebook&lc=white"><img src="https://img.shields.io/static/v1?label=&message=Facebook&color=1877F2&style=flat-square&logo=facebook&logoColor=white"></a>
+      <a href="?la=新元号は&me=令和であります&co=black&st=social&lo=Travis%20CI&lc=orange"><img src="https://img.shields.io/static/v1?label=%E6%96%B0%E5%85%83%E5%8F%B7%E3%81%AF&message=%E4%BB%A4%E5%92%8C%E3%81%A7%E3%81%82%E3%82%8A%E3%81%BE%E3%81%99&color=black&style=social&logo=travis-ci&logoColor=orange"></a>
+      <a href="?la=SuperDry%20極度乾燥&me=（しなさい）&co=red&st=plastic&lo=none&lc=orange"><img src="https://img.shields.io/static/v1?label=SuperDry%20%E6%A5%B5%E5%BA%A6%E4%B9%BE%E7%87%A5&message=%EF%BC%88%E3%81%97%E3%81%AA%E3%81%95%E3%81%84%EF%BC%89&color=red&style=plastic&logoColor=orange"></a>
+    </v-card-text>
+  </v-card>
+</template>
