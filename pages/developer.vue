130f1a623b9c6e0db8696ecfd099eedfe98d22bd
Futa Hirakoba <korosuke613613@gmail.com>
2020-01-07 20:11:13 +0900

    ちょっと修正

--- pages/developer.vue
+++ pages/developer.vue
@@ -8,12 +8,28 @@
       >
       <blockquote class="blockquote">
         I'm Futa. Student of Master's degree.<br>
-        I like <a href="https://www.karamenya-masumoto.com/"><img alt="Karamen" src="https://img.shields.io/static/v1?label=Karamen&message=%E8%BE%9B%E9%BA%BA&color=red&style=flat-square"></a>.
+        I like <a href="https://www.karamenya-masumoto.com/">Karamen -辛麺-</a>.
         <br><br>
-        <a href="https://korosuke613.github.io/"><img src="https://img.shields.io/static/v1?label=My&message=Homepage&color=purple&style=flat-square"></a>
-        <a href="https://github.com/korosuke613"><img src="https://img.shields.io/static/v1?label=&message=korosuke613&color=black&style=flat-square&logo=github"></a>
-        <a href="https://twitter.com/Shitimi_613"><img src="https://img.shields.io/static/v1?label=&message=@Shitimi_613&color=1DA1F2&style=flat-square&logo=twitter&logoColor=white"></a>
-        <a href="https://www.facebook.com/futa.hirakoba.5"><img src="https://img.shields.io/static/v1?label=&message=Futa HIRAKOBA&color=1877F2&style=flat-square&logo=facebook&logoColor=white"></a>
+        <a
+          target="_blank"
+          rel="noopener"
+          href="https://korosuke613.github.io/"
+        ><img src="https://img.shields.io/static/v1?label=My&message=Homepage&color=purple&style=flat-square"></a>
+        <a
+          target="_blank"
+          rel="noopener"
+          href="https://github.com/korosuke613"
+        ><img src="https://img.shields.io/static/v1?label=&message=korosuke613&color=black&style=flat-square&logo=github"></a>
+        <a
+          target="_blank"
+          rel="noopener"
+          href="https://twitter.com/Shitimi_613"
+        ><img src="https://img.shields.io/static/v1?label=&message=@Shitimi_613&color=1DA1F2&style=flat-square&logo=twitter&logoColor=white"></a>
+        <a
+          target="_blank"
+          rel="noopener"
+          href="https://www.facebook.com/futa.hirakoba.5"
+        ><img src="https://img.shields.io/static/v1?label=&message=Futa HIRAKOBA&color=1877F2&style=flat-square&logo=facebook&logoColor=white"></a>
       </blockquote>
       <span>&copy;2019 Futa HIRAKOBA </span>
     </v-flex>
